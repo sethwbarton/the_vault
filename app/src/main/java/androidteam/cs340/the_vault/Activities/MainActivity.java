9bd8ae4cd60f76c13abdb3f7d30ef5bfe21ec211
John McPhie <john.mcphie@hotmail.com>
2020-02-10 20:01:34 -0700

    Added database components

--- app/src/main/java/androidteam/cs340/the_vault/Activities/MainActivity.java
+++ app/src/main/java/androidteam/cs340/the_vault/Activities/MainActivity.java
@@ -1,11 +1,15 @@
 package androidteam.cs340.the_vault.Activities;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidteam.cs340.the_vault.Database.DatabaseHelper;
 import androidteam.cs340.the_vault.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +17,21 @@ public class MainActivity extends AppCompatActivity {
     /* Buttons */
     private Button dominionButton;
     private Button turnOrderButton;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this).getReadableDatabase();
+        //Example Code for reading from database:
+//        Cursor cursor = db.rawQuery("SELECT * FROM students", null);
+//        cursor.moveToFirst();
+//        while (!cursor.isAfterLast()) {
+//            Log.d("SQLITE", '\n' + cursor.getString(1));
+//            cursor.moveToNext();
+//        }
 
         /* Dominion Button */
         dominionButton = findViewById(R.id.dominionMainButton);
---
+++ app/src/main/java/androidteam/cs340/the_vault/Database/DatabaseHelper.java
@@ -0,0 +1,46 @@
+package androidteam.cs340.the_vault.Database;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "example.sqlite3";
+
+    private Context context;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        this.context = context;
+        installDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) { }
+
+    public void installDatabase() {
+        try {
+            InputStream inputStream = this.context.getAssets().open("databases/" + DATABASE_NAME);
+            File outputFile = new File(context.getDatabasePath(DATABASE_NAME).getPath());
+            OutputStream outputStream = new FileOutputStream(outputFile);
+
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+
+            outputStream.write(buffer);
+        }
+        catch (IOException e) {
+            Log.e("DEBUG", e.getMessage());
+        }
+    }
+}

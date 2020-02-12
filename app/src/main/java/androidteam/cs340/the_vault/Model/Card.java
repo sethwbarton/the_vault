7a461b609cc959b22e3727bffece699ed09bcae8
irae4 <irae483@gmail.com>
2020-02-12 13:22:16 -0700

    getter and setter on card

--- app/src/main/java/androidteam/cs340/the_vault/Model/Card.java
+++ app/src/main/java/androidteam/cs340/the_vault/Model/Card.java
@@ -5,4 +5,27 @@ public class Card {
     private String cost;
     private String type;
     private Enum<Expansion> expansion;
+
+    public Card(String name, String cost, String type, Enum<Expansion> expansion) {
+        this.name = name;
+        this.cost = cost;
+        this.type = type;
+        this.expansion = expansion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Enum<Expansion> getExpansion() {
+        return expansion;
+    }
 }

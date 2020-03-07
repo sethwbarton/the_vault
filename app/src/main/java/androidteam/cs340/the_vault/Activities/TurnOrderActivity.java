486fdd50d0cf6df62e4eb3140854d7de380b5cb9
Seth Barton <sethwbarton@gmail.com>
2020-03-07 13:40:38 -0700

    Turn oder activity shows up

--- app/src/main/java/androidteam/cs340/the_vault/Activities/TurnOrderActivity.java
+++ app/src/main/java/androidteam/cs340/the_vault/Activities/TurnOrderActivity.java
@@ -1,16 +1,29 @@
 package androidteam.cs340.the_vault.Activities;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import androidteam.cs340.the_vault.R;
 
 public class TurnOrderActivity extends AppCompatActivity {
 
+    private TextView titleText;
+    private TextView subTitleText;
+    private TextInputEditText playerNumberText;
+    private TextView turnOrderText;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn_order);
+
+        titleText =  findViewById(R.id.turnOrderTitle);
+        subTitleText = findViewById(R.id.turnOrderSubTitle);
+        playerNumberText =  findViewById(R.id.playerNumberPrompt);
+        turnOrderText = findViewById(R.id.turnOrder);
+
     }
 }

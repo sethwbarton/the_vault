84a86ab701cd96a81687aa207594a2e83f371e14
John McPhie <john.mcphie@hotmail.com>
2020-03-13 23:51:49 -0600

    Added D8 die to die activity

--- app/src/main/java/androidteam/cs340/the_vault/Activities/DiceActivity.java
+++ app/src/main/java/androidteam/cs340/the_vault/Activities/DiceActivity.java
@@ -30,13 +30,16 @@ public class DiceActivity extends AppCompatActivity {
     private final static int ROLL_DURATION = 1000;
     Button btnOneDie;
     Button btnTwoDice;
+    Button btnD8;
     ImageView imgDie1;
     ImageView imgDie2;
+    ImageView imgD8;
     TextView prompt;
     Random gen = new Random();
     SoundPool diceSound;
     int soundID;
-    Handler handler;
+    Handler handlerD6;
+    Handler handlerD8;
     Timer timer = new Timer();
     boolean isRolling = false;
     int numDice = 0;
@@ -50,16 +53,19 @@ public class DiceActivity extends AppCompatActivity {
         InitSound();
         prompt = (TextView) findViewById(R.id.dicePrompt);
         imgDie1 = (ImageView) findViewById(R.id.imageDie1);
-        imgDie1.setOnClickListener(new HandleClick());
+        imgDie1.setOnClickListener(new HandleClickD6());
         imgDie2 = (ImageView) findViewById(R.id.imageDie2);
-        imgDie2.setOnClickListener(new HandleClick());
+        imgDie2.setOnClickListener(new HandleClickD6());
+        imgD8 = (ImageView) findViewById(R.id.imageD8);
+        imgD8.setOnClickListener(new HandleClickD8());
+
 
         btnOneDie = (Button) findViewById(R.id.oneDieButton);
         btnOneDie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 numDice = 1;
-                switchViews();
+                displayD6();
             }
         });
         btnTwoDice = (Button) findViewById(R.id.twoDiceButton);
@@ -67,20 +73,41 @@ public class DiceActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 numDice = 2;
-                switchViews();
+                displayD6();
             }
         });
+        btnD8 = (Button) findViewById(R.id.d8Button);
+        btnD8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numDice = 1;
+                displayD8();
+            }
+        });
+
+
 
         rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotate.setDuration(ROLL_DURATION);
         rotate.setInterpolator(new LinearInterpolator());
-        handler = new Handler(callback);
+        handlerD6 = new Handler(callbackD6);
+        handlerD8 = new Handler(callbackD8);
     }
 
-    private void switchViews(){
+    private void removePrompt() {
         btnOneDie.setVisibility(View.GONE);
         btnTwoDice.setVisibility(View.GONE);
+        btnD8.setVisibility(View.GONE);
         prompt.setVisibility(View.GONE);
+    }
+
+    private void displayD8() {
+        removePrompt();
+        imgD8.setVisibility(View.VISIBLE);
+    }
+
+    private void displayD6(){
+        removePrompt();
 
         imgDie1.setVisibility(View.VISIBLE);
         if (numDice > 1) {
@@ -93,7 +120,7 @@ public class DiceActivity extends AppCompatActivity {
         }
     }
 
-    private class HandleClick implements View.OnClickListener {
+    private class HandleClickD6 implements View.OnClickListener {
         public void onClick(View arg0) {
             if (!isRolling) {
                 isRolling = true;
@@ -101,7 +128,18 @@ public class DiceActivity extends AppCompatActivity {
                 imgDie1.startAnimation(rotate);
                 imgDie2.setImageResource(R.drawable.die);
                 imgDie2.startAnimation(rotate);
-                timer.schedule(new Roll(), ROLL_DURATION);
+                timer.schedule(new RollD6(), ROLL_DURATION);
+            }
+        }
+    }
+
+    private class HandleClickD8 implements View.OnClickListener {
+        public void onClick(View arg0) {
+            if (!isRolling) {
+                isRolling = true;
+                imgD8.setImageResource(R.drawable.d8_1);
+                imgD8.startAnimation(rotate);
+                timer.schedule(new RollD8(), ROLL_DURATION);
             }
         }
     }
@@ -117,9 +155,46 @@ public class DiceActivity extends AppCompatActivity {
         soundID = diceSound.load(this, R.raw.shake_dice,1);
     }
 
-    class Roll extends TimerTask {
+    class RollD6 extends TimerTask {
         public void run() {
-            handler.sendEmptyMessage(0);
+            handlerD6.sendEmptyMessage(0);
+        }
+    }
+
+    class RollD8 extends TimerTask {
+        public void run() {
+            handlerD8.sendEmptyMessage(0);
+        }
+    }
+
+    private void setD8(ImageView die, int num){
+        rotate.cancel();
+        switch(num) {
+            case 1:
+                die.setImageResource(R.drawable.d8_1);
+                break;
+            case 2:
+                die.setImageResource(R.drawable.d8_2);
+                break;
+            case 3:
+                die.setImageResource(R.drawable.d8_3);
+                break;
+            case 4:
+                die.setImageResource(R.drawable.d8_4);
+                break;
+            case 5:
+                die.setImageResource(R.drawable.d8_5);
+                break;
+            case 6:
+                die.setImageResource(R.drawable.d8_6);
+                break;
+            case 7:
+                die.setImageResource(R.drawable.d8_7);
+                break;
+            case 8:
+                die.setImageResource(R.drawable.d8_8);
+                break;
+            default:
         }
     }
 
@@ -148,11 +223,20 @@ public class DiceActivity extends AppCompatActivity {
         }
     }
 
-    Handler.Callback callback = new Handler.Callback() {
+    Handler.Callback callbackD6 = new Handler.Callback() {
         public boolean handleMessage(Message msg) {
             playSound = diceSound.play(soundID, 1.0f, 1.0f, 0, 0, 1.0f);
             setDie(imgDie1, gen.nextInt(6)+1);
             setDie(imgDie2, gen.nextInt(6)+1);
+            isRolling=false;
+            return true;
+        }
+    };
+
+    Handler.Callback callbackD8 = new Handler.Callback() {
+        public boolean handleMessage(Message msg) {
+            playSound = diceSound.play(soundID, 1.0f, 1.0f, 0, 0, 1.0f);
+            setD8(imgD8, gen.nextInt(8)+1);
             isRolling=false;
             return true;
         }
